e878815c1bf00b1b46ca4847eceb7df979d25279
zerozayu <304307556@qq.com>
2022-04-01 23:51:55 +0800

    feat: 根据用户id查询菜单列表

---
+++ yeb-server/src/main/java/com/zhangyu/server/controller/MenuController.java
@@ -0,0 +1,32 @@
+package com.zhangyu.server.controller;
+
+import com.zhangyu.server.pojo.Menu;
+import com.zhangyu.server.service.AdminService;
+import com.zhangyu.server.service.MenuService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 前端控制器
+ *
+ * @author zhangyu
+ * @date 2022/4/1 22:21
+ */
+@RestController
+@RequestMapping("/system/cfg")
+public class MenuController {
+
+    @Autowired
+    private MenuService menuService;
+
+    @ApiOperation(value = "通过用户id查询菜单列表")
+    @GetMapping("/menu")
+    public List<Menu> getMenusByAdminId(){
+        return menuService.getMenusByAdminId();
+    }
+}
