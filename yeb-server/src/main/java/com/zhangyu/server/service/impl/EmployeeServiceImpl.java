00a12545b8ade9a3c6d33608d661fc7230e7632f
zerozayu <304307556@qq.com>
2022-04-11 21:04:48 +0800

    feat: 新增获取所有员工分页查询

--- yeb-server/src/main/java/com/zhangyu/server/service/impl/EmployeeServiceImpl.java
+++ yeb-server/src/main/java/com/zhangyu/server/service/impl/EmployeeServiceImpl.java
@@ -1,19 +1,45 @@
 package com.zhangyu.server.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.zhangyu.server.pojo.Employee;
-import com.zhangyu.server.service.EmployeeService;
 import com.zhangyu.server.mapper.EmployeeMapper;
+import com.zhangyu.server.pojo.Employee;
+import com.zhangyu.server.pojo.RespPageBean;
+import com.zhangyu.server.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 /**
-* @author zhangyu
-* @description 针对表【t_employee】的数据库操作Service实现
-* @createDate 2022-03-07 21:17:10
-*/
+ * @author zhangyu
+ * @description 针对表【t_employee】的数据库操作Service实现
+ * @createDate 2022-03-07 21:17:10
+ */
 @Service
 public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee>
-    implements EmployeeService{
+        implements EmployeeService {
+
+    @Autowired
+    private EmployeeMapper employeeMapper;
+
+    /**
+     * 获取所有员工信息（分页）
+     *
+     * @param currentPage
+     * @param size
+     * @param employee
+     * @param beginDateScope
+     * @return
+     */
+    @Override
+    public RespPageBean getEmployee(Integer currentPage, Integer size, Employee employee, LocalDate[] beginDateScope) {
+        // 开启分页
+        Page<Employee> page = new Page<>(currentPage, size);
+        IPage<Employee> employeeByPage = employeeMapper.getEmployeeByPage(page, employee, beginDateScope);
+        return new RespPageBean(employeeByPage.getTotal(), employeeByPage.getRecords());
+    }
 
 }
 
